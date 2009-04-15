302b6dd92abbe4062480867397fd316645975f01
Lukas Linhart <lukas.linhart@centrumholdings.com>
2009-04-15 13:24:11 +0200

    Deleted RichTextField registration for SourceText - this should be done for target model.

--- djangomarkup/admin.py
+++ djangomarkup/admin.py
@@ -1,21 +1,5 @@
 from django.contrib.sites.admin import admin
-from django.contrib.admin.options import ModelAdmin
-
 from djangomarkup.models import SourceText, TextProcessor
-from djangomarkup.fields import RichTextField
 
-class SourceTextOptions(ModelAdmin):
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == "content":
-            kwargs.update({
-                'required': not db_field.blank,
-                'label': db_field.verbose_name,
-                'field_name': db_field.name,
-                'instance': kwargs.get('instance', None),
-                'model': self.model,
-            })
-            return RichTextField(**kwargs)
-        return super(SourceTextOptions, self).formfield_for_dbfield(db_field, **kwargs)
-
-admin.site.register(SourceText, SourceTextOptions)
+admin.site.register(SourceText)
 admin.site.register(TextProcessor)
