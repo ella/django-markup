b8c0fdb530359408f693f3636a085a778f65905f
Lukas Linhart <lukas.linhart@centrumholdings.com>
2009-04-17 17:23:29 +0200

    SourceText is not saved on form_clean, but only after save of original object (prevents problems when field is cleared, but form do not validate)

--- tests/example_project/tests/test_admin/test_article_markup.py
+++ tests/example_project/tests/test_admin/test_article_markup.py
@@ -3,7 +3,7 @@ from helpers import AdminTestCase
 
 class TestMarkup(AdminTestCase):
 
-    def test_markup_saving(self):
+    def test_markup_successfull_saving(self):
         s = self.selenium
         text = u"An example text in markdown markup"
 
@@ -11,7 +11,9 @@ class TestMarkup(AdminTestCase):
         s.click(self.elements['pages']['welcome']['exampleapp_article_add'])
 
         s.type('id_text', text)
+        s.type('id_email', 'examplemail@example.com')
         s.click(self.elements['listing']['save'])
+        s.wait_for_page_to_load(3000)
 
         assert "<p>%s</p>" % text in s.get_text(self.elements['listing']['list_first'])
 
@@ -19,3 +21,18 @@ class TestMarkup(AdminTestCase):
 
         self.assert_equals(text, s.get_value('id_text'))
 
+    def test_unsuccessfull_save_do_not_generate_sourcetext(self):
+        s = self.selenium
+        text = u"An example text in markdown markup"
+
+        s.click(self.elements['pages']['welcome']['exampleapp_article_add'])
+
+        s.type('id_text', text)
+        s.click(self.elements['listing']['save'])
+        s.wait_for_page_to_load(3000)
+
+        s.click(self.elements['navigation']['home'])
+
+        s.click(self.elements['pages']['welcome']['djangomarkup_text_sources'])
+
+        self.assert_equals(u"0 Text sources", s.get_text(self.elements['listing']['count_field']))
