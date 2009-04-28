0b057e5ea33549e5ef1ec1cabeb2bcf50a63bf6a
Vitek Pliska <whit@jizak.cz>
2009-04-28 15:43:09 +0200

    View for preview rendered (x)HTML.

---
+++ djangomarkup/views.py
@@ -0,0 +1,30 @@
+import logging
+
+from django import template
+from django.conf import settings
+from django.http import HttpResponse
+from django.utils.translation import ugettext
+
+from djangomarkup.models import TextProcessor
+
+log = logging.getLogger('djangomarkup')
+
+def preview(request, var_name="text", syntax_processor_name=None):
+    """
+    Returns rendered HTML for source text
+    """
+    source = request.REQUEST.get(var_name)
+    if not source:
+        return HttpResponse('')
+
+    processor = TextProcessor.objects.get(name=syntax_processor_name or getattr(settings, "DEFAULT_MARKUP", "markdown"))
+    output = processor.convert(source)
+
+    try:
+        t = template.Template(output, name='markup_preview')
+        output = t.render(template.Context({'MEDIA_URL' : settings.MEDIA_URL}))
+    except template.TemplateSyntaxError, e:
+        log.error('Error in preview rendering: %s' % e)
+        output = '<h3 style="color:red">%s</h3><p>%s</p>' % (ugettext('You have an errors in source text!'), e)
+
+    return HttpResponse(output)
