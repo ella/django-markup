a72f71366bc82fd4ebb88a56bb4308680382492e
Honza Kral <honza.kral@gmail.com>
2009-06-29 13:24:00 +0200

    fixed tests

--- tests/unit_project/urls.py
+++ tests/unit_project/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls.defaults import patterns, url, include, handler500, handler404
 
 urlpatterns = patterns('',
     url(r'^', include('djangomarkup.urls')),
